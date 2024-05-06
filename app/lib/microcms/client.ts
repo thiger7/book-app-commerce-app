3d399e74938a8a10017274b27f10efdeec3aa1a7
thiger7 <adf0423@gmail.com>
2024-05-06 13:41:55 +0900

    電子記事用の型を用意する

--- app/lib/microcms/client.ts
+++ app/lib/microcms/client.ts
@@ -1,6 +1,14 @@
-import { createClient } from 'microcms-js-sdk';
+import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
-  serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN!, 
+  serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN!,
   apiKey: process.env.NEXT_PUBLIC_API_KEY!,
 });
+
+export const getAllBooks = async () => {
+  const allBooks = await client.getList({
+    endpoint: "bookcommerce",
+  });
+
+  return allBooks;
+};
