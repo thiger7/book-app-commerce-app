57187a4c7c5da57c2fe0f800f5f0d47e0cd551b3
thiger7 <adf0423@gmail.com>
2024-05-08 09:40:32 +0900

    buildコマンドに prisma generateを追加する

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
   const session = await getServerSession(nextAuthOptions);
   const user = session?.user as User;
 
-  let purchaseBookIds: object[] = [];
+  let purchaseBookIds: string;
 
   if (user) {
     const response = await fetch(
