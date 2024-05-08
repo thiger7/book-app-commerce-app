bff3407106fef909b57fc8a020a42ccc1d9920cd
thiger7 <adf0423@gmail.com>
2024-05-08 11:24:58 +0900

    購入成功ページにloading処理を追加

--- app/book/checkout-success/page.tsx
+++ app/book/checkout-success/page.tsx
@@ -3,14 +3,17 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import Loading from "@/app/loading";
 
 const PurchaseSuccess = () => {
   const [bookUrl, setBookUrl] = useState(null);
   const searchParams = useSearchParams();
   const sessionId = searchParams.get("session_id");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       if (sessionId) {
         try {
           const res = await fetch(
@@ -25,6 +28,7 @@ const PurchaseSuccess = () => {
           const data = await res.json();
           console.log(data);
           setBookUrl(data.purchase.bookId);
+          setIsLoading(false);
         } catch (err) {
           console.error(err);
         }
@@ -34,24 +38,26 @@ const PurchaseSuccess = () => {
   }, []);
 
   return (
-    <div className="flex items-center justify-center bg-gray-100 mt-20">
-      <div className="bg-white p-6 rounded-lg shadow-lg">
-        <h1 className="text-2xl font-bold text-center text-gray-800 mb-4">
-          購入ありがとうございます！
-        </h1>
-        <p className="text-center text-gray-600">
-          ご購入いただいた内容の詳細は、登録されたメールアドレスに送信されます。
-        </p>
-        <div className="mt-6 text-center">
-          <Link
-            href={`/book/${bookUrl}`}
-            className="text-indigo-600 hover:text-indigo-800 transition duration-300"
-          >
-            購入した記事を読む
-          </Link>
+    (isLoading && <Loading />) || (
+      <div className="flex items-center justify-center bg-gray-100 mt-20">
+        <div className="bg-white p-6 rounded-lg shadow-lg">
+          <h1 className="text-2xl font-bold text-center text-gray-800 mb-4">
+            購入ありがとうございます！
+          </h1>
+          <p className="text-center text-gray-600">
+            ご購入いただいた内容の詳細は、登録されたメールアドレスに送信されます。
+          </p>
+          <div className="mt-6 text-center">
+            <Link
+              href={`/book/${bookUrl}`}
+              className="text-indigo-600 hover:text-indigo-800 transition duration-300"
+            >
+              購入した記事を読む
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
