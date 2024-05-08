46f708584b8db5d64ff68a5f247d54b213d4bd58
thiger7 <adf0423@gmail.com>
2024-05-08 11:02:32 +0900

    購入成功ページに正常にリダイレクトするようにする

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -60,7 +60,7 @@ const Book = ({ book, isPurchased }: BookProps) => {
     if (!user) {
       setShowModal(false);
       //ログインページへリダイレクト
-      router.push("/login");
+      router.push("/api/auth/signin");
     } else {
       //Stripeで決済する
       startCheckout();
